b8dd6c29e146742f5a3a5be0e4726792d2d9458c
lamnaourm <mohammed.lamnaour@gmail.com>
2022-10-31 13:46:59 +0100

    Style CSS

--- src/components/Produit/Produit.jsx
+++ src/components/Produit/Produit.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './produit.module.css'
 
 
-const Produit = () => {
+const Produit = (props) => {
     return (
         <div className={styles.produit}>
             <img src={props.produit.images[0]} alt={props.produit.title}/>
             <h1>{props.produit.title}</h1>
             <p>{props.produit.description}</p>
-            <h2>Prix : {props.produit.price} DH</h2>
+            <h3>Prix : {props.produit.price} DH</h3>
             <button>Ajouter au panier</button>
         </div>
     );
--- src/components/Produits/produits.jsx
+++ src/components/Produits/produits.jsx
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
 import styles from "./produits.module.css";
 import axios from "axios";
+import Produit from "../Produit/Produit";
 
 const Produits = (props) => {
-    const [categories, setCategories] = useState([]);
-    const [catId, setCatId] = useState(0);
+  const [categories, setCategories] = useState([]);
+  const [produits, setProduits] = useState([]);
+  const [catId, setCatId] = useState(0);
 
   useEffect(() => {
     const getData = async () => {
       const res = await axios.get("https://api.escuelajs.co/api/v1/categories");
       return res.data;
     };
-
     getData().then((cats) => setCategories(cats));
   }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      let api = "";
+
+      if (catId === 0) 
+        api = "https://api.escuelajs.co/api/v1/products";
+      else 
+        api = `https://api.escuelajs.co/api/v1/categories/${catId}/products`;
+
+      const res = await axios.get(api);
+      return res.data;
+    };
+    getData().then((prods) => setProduits(prods));
+  }, [catId]);
+
   return (
-    <div className={styles.list}>
+    <div className={styles.produits}>
       <select value={catId} onChange={(e) => setCatId(e.target.value)}>
         <option value={0}>Tous les produits</option>
         {categories.map((item) => (
@@ -24,6 +41,14 @@ const Produits = (props) => {
           </option>
         ))}
       </select>
+
+      <div className={styles.list}>
+        {produits.length === 0 ? (
+          <h1>Liste produits vide</h1>
+        ) : (
+          produits.map((p) => <Produit produit={p} />)
+        )}
+      </div>
     </div>
   );
 };
